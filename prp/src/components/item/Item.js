8d02339b3ca4de93dbc8d24da97aadb7e7255a51
Elghorez <ellamjed@gmail.com>
2021-05-13 03:39:36 -0700

    default props

--- prp/src/components/item/Item.js
+++ prp/src/components/item/Item.js
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const Item = ({item}) => {
-    const product  =(name,price)=> {
-        alert(name, price)}
+    const product  =(nameprice)=> {
+        alert(nameprice)}
     return (
-        <>
-            <tr>
+    <>
+     <tr>
       <th scope="row"><button  onClick={() =>product(item.name +" "+item.price)}>Product Details</button></th>
       <td><img src={item.imgSrc} alt={item.name} style={{width:"200px"}}/></td>
       <td>{item.name}</td>
